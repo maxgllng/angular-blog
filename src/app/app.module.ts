ba3323fe03105d074ffca6943c4b30f3123698fd
Max Gylling <max.gylling@utb.ecutbildning.se>
2025-01-07 10:48:14 +0100

    Raderat oanvända komponenter i app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutUserComponent } from './pages/about-user/about-user.component';
-import { AboutAdminComponent } from './pages/about-admin/about-admin.component';
-import { HomeUserComponent } from './pages/home-user/home-user.component';
-import { HomeAdminComponent } from './pages/home-admin/home-admin.component';
-import { BlogUserComponent } from './pages/blog-user/blog-user.component';
-import { BlogAdminComponent } from './pages/blog-admin/blog-admin.component';
 import { CreatePostComponent } from './pages/create-post/create-post.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -19,12 +13,6 @@ import { BlogComponent } from './pages/blog/blog.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUserComponent,
-    AboutAdminComponent,
-    HomeUserComponent,
-    HomeAdminComponent,
-    BlogUserComponent,
-    BlogAdminComponent,
     CreatePostComponent,
     NavbarComponent,
     HeaderComponent,
