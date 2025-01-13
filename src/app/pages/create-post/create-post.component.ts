efddee1cc2308bfe480736e638798ead20a08f54
Max Gylling <max.gylling@utb.ecutbildning.se>
2025-01-13 16:07:57 +0100

    Lagt till formulär för create-post

--- src/app/pages/create-post/create-post.component.ts
+++ src/app/pages/create-post/create-post.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
   standalone: false,
-  
+
   templateUrl: './create-post.component.html',
-  styleUrl: './create-post.component.css'
+  styleUrl: './create-post.component.css',
 })
 export class CreatePostComponent {
-
+  newPost = new FormGroup({
+    title: new FormControl(''),
+    imgUrl: new FormControl(''),
+    body: new FormControl(''),
+  });
 }
